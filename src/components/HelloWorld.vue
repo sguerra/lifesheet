5cb29e508cf9c14140e8a70fe414d51b71ad5e94
Sergio Guerra <sergioguerra1991@gmail.com>
2023-01-26 01:43:33 -0600

    feat: Setup tailwind css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,34 +7,13 @@ const count = ref(0)
 </script>
 
 <template>
-	<h1>{{ msg }}</h1>
-
-	<div class="card">
-		<button type="button" @click="count++">count is {{ count }}</button>
-		<p>
-			Edit
-			<code>components/HelloWorld.vue</code> to test HMR
-		</p>
+	<div class="h-96 justify-between flex flex-col">
+		<h1 class="text-6xl flex-grow">{{ msg }}</h1>
+		<section class="flex flex-col">
+			<h2 class="text-2xl">Start here:</h2>
+			<textarea rows="10" cols="100"></textarea>
+		</section>
 	</div>
-
-	<p>
-		Check out
-		<a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-			>create-vue</a
-		>, the official Vue + Vite starter
-	</p>
-	<p>
-		Install
-		<a href="https://github.com/johnsoncodehk/volar" target="_blank"
-			>Volar</a
-		>
-		in your IDE for a better DX
-	</p>
-	<p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-	color: #888;
-}
-</style>
+<style scoped></style>
